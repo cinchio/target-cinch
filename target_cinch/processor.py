7f2d0bdb1401be84a8fe63234f929a0fd43e5215
Chris London <me@chrislondon.co>
2021-09-22 08:55:56 -0600

    remove bad print

--- target_cinch/processor.py
+++ target_cinch/processor.py
@@ -53,8 +53,7 @@ class Processor:
         elif model == "transaction_detail":
             self.service.post_transaction_details(self.batch_queues[model])
         elif model == "engagement":
-            res = self.service.post_engagements(self.batch_queues[model])
-            print(res.text, flush=True)
+            self.service.post_engagements(self.batch_queues[model])
 
         self.batch_queues[model] = []
 
